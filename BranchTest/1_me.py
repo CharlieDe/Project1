fe4734c48364b1233327c9d999fda47314c66224
charliep <charlito28art@gmail.com>
2019-07-09 19:25:53 -0500

    branch commit

---
+++ BranchTest/1_me.py
@@ -0,0 +1,20 @@
+word = "Murcielago"
+
+letters = [letter for letter in word]
+print (letters)
+
+def show():
+    print("Hi")
+    
+show()
+
+def deletreo(word):
+    lperl = [letter for letter in word]
+    print (lperl)
+    
+deletreo("hola")
+
+deletreo("miercoles")
+deletreo(" hoy es sabado")
+
+
